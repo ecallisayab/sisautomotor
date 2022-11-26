f519a87da29ff4ebb32f382bbc96d1d7d23c0bd8
Edwin Callisaya Bautista <ecallisayab@gmail.com>
2022-11-25 23:25:28 -0400

    Ajustes página principal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,9 +11,7 @@
                 <div class="text-center">
                     <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 50rem;" src="{{ asset('custom/img/municipio_santiago_huata.jpg') }}" alt="...">
                 </div>
-                <p>Add some quality, svg illustrations to your project courtesy of <a target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a
-                    constantly updated collection of beautiful svg images that you can use
-                    completely free and without attribution!</p>
+                <p>Santiago de Huata es un pueblo y municipio de Bolivia, ubicado a orillas del lago Titicaca, dentro de la provincia de Omasuyos en el departamento de La Paz. Geográficamente conforma una región y península al este del lago Titicaca.</p>
             </div>
         </div>
     </div>
@@ -26,9 +24,7 @@
                 <div class="text-center">
                 <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 50rem;" src="{{ asset('custom/img/maquinaria_pesada.jpg') }}" alt="...">
                 </div>
-                <p>Add some quality, svg illustrations to your project courtesy of <a target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a
-                    constantly updated collection of beautiful svg images that you can use
-                    completely free and without attribution!</p>
+                <p>El sistema de control y seguimiento del parque automotor (SIS.AUTOMOTOR), permitirá manejar de manera más eficiente el recurso combustible, ayudando a controlar su uso y disponibilidad. Asimismo, ayudará a controlar el mantenimiento de los vehículos existentes, para que estén funcionales y operativos.</p>
             </div>
         </div>
     </div>
